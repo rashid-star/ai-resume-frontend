2b90e9e5667d5283eee9faf59321642e23c7def0
Mohammad Rashid <chaudharyrasid31@gmail.com>
2026-04-23 17:29:48 +0530

    change api to railway production to render

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://ai-resume-backend-3127.onrender.com"
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
